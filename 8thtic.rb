23cdbf7cb117031bfce357411d8e8020ce5f534d
Greg Heaphy <heaphyg@gmail.com>
2014-05-31 16:57:10 -0500

    made sure that player1 and player2 are now defined and started implementing the opponent_turn methods which currently requires other methods to be defined in order for it to work.

--- 8thtic.rb
+++ 8thtic.rb
@@ -5,9 +5,9 @@ class TicTacToe
     @turn_choice = nil
     @opponent_piece = nil
     @board_spaces = {     # defining all 9 board spaces and setting them as unoccupied with empty strings
-      1 => " ",2 => " ",3 => " ",
-      4 => " ",5 => " ",6 => " ",
-      7 => " ",8 => " ",9 => " "
+      1 => "1",2 => "2",3 => "3",
+      4 => "4",5 => "5",6 => "6",
+      7 => "7",8 => "8",9 => "9"
     }
 
     @potential_victory_scenarios = [
@@ -27,6 +27,7 @@ class TicTacToe
      prompt
      user_turn_choice
      assign_opponent_piece
+     turn # this may not be nessecary
      print_board
   end
 
@@ -43,13 +44,17 @@ class TicTacToe
         puts "please write an 'X' if you would like to go first or an 'O' if you would like to go second"
         self.turn_choice = gets.chomp.upcase
       elsif turn_choice  == 'X'
-        puts "Excellent #{user_name}. You have chosen to go first. Take your turn."
+        puts "Excellent #{user_name}. You have chosen to go first. Please select the number of the space you wish to occupy."
         proper_piece_selection = true
-        turn_choice  # im concerned that this could be DRYer
+        player1 = user_name
+        player2 = "Computer"
+        # turn_choice  # im concerned that this could be DRYer
       else
         puts "#{user_name} you have chosen to go second. Bold move."
         proper_piece_selection = true
-        turn_choice # we may not even need these here
+        player1 = "Computer"
+        player2 = user_name
+        # turn_choice # we may not even need these here
       end
     end
   end
@@ -64,18 +69,24 @@ class TicTacToe
 
   def turn
     if turn_choice == 'X'
-      user_turn
+      # user_turn
     else
-      opponent_turn
+      # opponent_turn
     end
   end
 
   def print_board  ## perhaps the board should have numbers printed in it..
-    puts " 1 #{board_spaces[1]}|#{board_spaces[2]}|#{board_spaces[3]}"
-    puts "   -----"
-    puts " 2 #{board_spaces[4]}|#{board_spaces[5]}|#{board_spaces[6]}"
-    puts "   -----"
-    puts " 3 #{board_spaces[7]}|#{board_spaces[8]}|#{board_spaces[9]}"
+    puts "                    #{board_spaces[1]}|#{board_spaces[2]}|#{board_spaces[3]}"
+    puts "                    -----"
+    puts "                    #{board_spaces[4]}|#{board_spaces[5]}|#{board_spaces[6]}"
+    puts "                    -----"
+    puts "                    #{board_spaces[7]}|#{board_spaces[8]}|#{board_spaces[9]}"
+  end
+
+  def opponent_turn
+    move = opponenet_determine_move  # this method will eventually choose the best move to make if there is a best move
+    board_spaces[move] = opponent_piece
+    check_game()
   end
 end
 
